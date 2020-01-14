577a3e494acde59034450066a4fbe79e3cc1f974
bassem jadoui <36534106+bassem97@users.noreply.github.com>
2020-01-14 19:18:55 +0100

    sign in up

--- src/app/sign/sign.component.ts
+++ src/app/sign/sign.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {NavbarComponent} from '../navbar/navbar.component';
 import {ActivatedRoute, Router} from '@angular/router';
-import {FormControl, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Agency} from '../Models/Agency';
+import {Client} from '../Models/Client';
 
 @Component({
   selector: 'app-sign',
@@ -12,18 +14,48 @@ export class SignComponent implements OnInit {
   selected: number;
   type = new FormControl('', Validators.required);
   selectedItem = 'Person';
+  agency = new Agency();
+  client = new Client();
+  signUpForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    // choosing sign in or sign up depanding on button clicked in navbar
     this.route.paramMap.subscribe(params => {
       this.selected = Number(params.get('id'));
     });
+    // form control
+    this.signUpForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      userName: ['', [Validators.required]],
+      taxRegistration: ['', [Validators.required]],
+      agencyName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6) ]],
+      rePassword: ['', Validators.required] }, {
+      validator: MustMatch('password', 'rePassword')
+    });
 
   }
+}
 
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (formGroup: FormGroup) => {
+    const control = formGroup.controls[controlName];
+    const matchingControl = formGroup.controls[matchingControlName];
 
-  closeSelection() {
-    console.log(this.selectedItem);
-  }
+    if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+      // return if another validator has already found an error on the matchingControl
+      return;
+    }
+
+    // set error on matchingControl if validation fails
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  };
 }
